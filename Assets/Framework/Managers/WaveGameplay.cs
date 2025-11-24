19108e80253fd6648cc21e15c67de0c8857cefd9
geia <111124706+geheyah@users.noreply.github.com>
2025-11-24 13:12:01 +0800

    Update WaveGameplay.cs

--- Assets/Framework/Managers/WaveGameplay.cs
+++ Assets/Framework/Managers/WaveGameplay.cs
@@ -9,7 +9,7 @@ public class WaveGameplay : MonoBehaviour
     
     public int enemieskilled = 0;
     public int enemiesrequired = 10;
-    public TMP_Text current,maximum;
+    public TMP_Text counter;
     
     public UnityEvent<int> WaveChanged = new UnityEvent<int>();
 
@@ -41,7 +41,7 @@ public class WaveGameplay : MonoBehaviour
     public void Update()
     {
         this.NewWave();
-        current.text = enemieskilled.ToString();
-        maximum.text = enemiesrequired.ToString();
+        counter.text = enemieskilled + "/"+ enemiesrequired.ToString();
+        
     }
 }
